5764d5a28f91c2af9f48cc4a4276f661ec4daa84
Vo-Ba-Nguyen <80742814+Vo-Ba-Nguyen@users.noreply.github.com>
2021-04-02 19:46:46 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,37 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Login</title>
-    <link rel="stylesheet" href="fontawesome-free/css/all.css">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+    <title>NguyenVB Shop</title>
+    <link rel="stylesheet" type="text/css" href="phi2.css">
+    <link rel="stylesheet" href="https://l.facebook.com/l.php?u=https%3A%2F%2Fopenplanning.net%2F11745%2Fbootstrap%3Ffbclid%3DIwAR2pyRn0xwkFsW9i8FwZQ3A8u5p679cH8YssRvbQR8g52kPbZhRJuA1Wg18&h=AT1t9RH5wUcyep6ZUBYHz0UNjJQ7XfdFWM4qd30GrdbrLMxKVvYdgK3VdAu21pK3GhE0Gw5JFO7NZH6qw-YaOSL9Rhr3IV9l6UagXSvviPQLWejrs2JnPpTaz0MPY5qFuhbW-w">  
 </head>
 <body>
-    <div id="main">
-        <div id="header">
-            <h2 class="brand-name text-style header-nav">Travel & Co</h2>
-            <button type="submit" class="text-style header-nav">Sign up</button>
-        </div>
-        <div id="content">
-            <h1 class="content-desc text-style">Travel</h1>
-        </div>
-        <div id="footer">
-            <div id="left-footer-content">
-                <h2 class="number-1">03</h2>
-                <h3 class="number-2">/07</h3>
-            </div>
-            <div id="right-footer-content">
-                <i class="icons-style fab fa-youtube"></i>
-                <i class="icons-style fab fa-instagram"></i>
-                <i class="icons-style fab fa-snapchat-ghost"></i>
+    <header> <img src="Background.jpg" width="100%" height="300px"/>
+    </header>
+    <div id="navigation">
+        <nav id="topmenu">
+            <ul>
+              <li><a class="navbar-brand" href="#">Home</a></li>
+              <li><a href="#">Information</a></li>
+              <li><a href="#">Products</a></li>
+              <li><a href="#">Numbers</a></li>
+              <li><a href="#">Others</a></li>
+            </ul>
+          </nav>
+        </div id ="header">
+    <section>
+        <article>
+            <h1> Inspection </h1>
+            <p>Mirum est notare quam littera gothica,quam nunc putamus parum claram</p>
+            <div class="price"> $300.000 </div>
+            <ul>
+                <img src="chuotmaytinh.jpg">
+                <li> Model and Model Wired Computer Mouse</li>
+                <li>Product dimensions (D x W x H cm) 10 x 7 x 3</li>
+                <li>Weight (KG) 0.04</li>
+                <li>Compact size, easy to carry with you</li>
+                <li>With a responsive vertical scroll wheel and 2 flexible double left, double right buttons to help you use the most easily.</li>
+            </ul>
+            <button>Contact me</button>
+        </article>
+        <article>
+            <h1> Inspection </h1>
+            <p>Mirum est notare quam littera gothica,quam nunc putamus parum claram</p>
+            <div class="price"> $300.000 </div>
+            <ul>
+                <img src="chuot2.jpg">
+                <li> 2.4G wireless signal transmission distance up to 8 meters with USB receiver</li>
+                <li>Oil rubber coating helps to prevent the slippery caused by perspiration from the user's hands</li>
+                <li>Product model: WH109</li>
+                <li>Size: 110 * 70 * 37mm</li>
+                <li>Source: 2 x AAA batteries</li>
+            </ul>
+            <button>Contact me</button>
+        </article>
 
-            </div>
-        </div>
+    </section>
+    
 </body>
-
 </html>
